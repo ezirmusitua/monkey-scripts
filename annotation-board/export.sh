d36456df520c2ed4052127bb9016426947d3f07f
ezirmusitua <jferroal@gmail.com>
2017-07-30 10:49:58 +0800

    Update annotation sh script
    Took 1 minute

--- annotation-board/export.sh
+++ annotation-board/export.sh
@@ -4,20 +4,21 @@ echo "../node_modules/.bin/browserify ./src/index.js -o ./dist/bundle.js"
 echo " = = = = = Browserify done = = = = = = "
 echo "prepend greasyfork head "
 sed -i.old '1s;^;\// ==UserScript==\
-// @name                highlight-my-interest\
-// @name:zh-CN          高亮关键词\
-// @description         highlight keywords in my favorites\
-// @description:zh-CN   高亮特定网页中感兴趣的关键词\
-// @version             0.1.8\
+// ==UserScript==
+// @name                annotation-board\
+// @name:zh-CN          注释墙\
+// @description         allow you to add annotation after selected content and copy to clipboard and save to local server\
+// @description:zh-CN   选中内容后添加注释并复制到剪贴板, 同时在本地的服务其中新建一个副本, 参见 https://github.com/ezirmusitua/snippet-board\
+// @version             0.1.5\
 // @author              jferroal\
 // @license             GPL-3.0\
-// @grant               none\
+// @updateURL           https://github.com/ezirmusitua/my-tamper-monkey-scripts/raw/master/annotation-board.user.js\
 // @require             https://greasyfork.org/scripts/31793-jmul/code/JMUL.js?version=208363\
-// @include             https://sspai.com/*\
-// @include             https://toutiao.io/*\
-// @include             http://www.inoreader.com/*\
-// @run-at              document-end\
-// @namespace           https://greasyfork.org/users/34556-jferroal\
+// @include             http://*\
+// @include             https://*\
+// @grant               GM_xmlhttpRequest\
+// @run-at              document-start\
+// @namespace           https://greasyfork.org/users/34556\
 // ==/UserScript==\n\n;' ./dist/bundle.js
 echo " = = = = = prepend bundle with greasyfork head done = = = = = "
 rm ./dist/bundle.js.old
