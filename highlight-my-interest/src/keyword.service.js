5e76a27fbec36bfccf767bf6ee29b4972b205f1d
ezirmusitua <jferroal@gmail.com>
2017-08-06 15:56:40 +0800

    Fix bug
    Took 6 minutes

--- highlight-my-interest/src/keyword.service.js
+++ highlight-my-interest/src/keyword.service.js
@@ -1,13 +1,15 @@
 class KeywordService {
   static init (setting, href) {
     KeywordService.Setting = setting;
+    KeywordService.keywords = [];
     const sites = Object.keys(KeywordService.Setting.keywords);
-    if (sites && sites.length) {
-      KeywordService.keywords = sites.reduce((res, site) => {
-        const sitePattern = new RegExp(site, 'gi');
-        return res.concat((sitePattern.test(href) && KeywordService.Setting.keywords[ site ]) || []);
-      }, []);
-    }
+    if (!sites || !sites.length) return;
+    sites.forEach((site) => {
+      const sitePattern = new RegExp(site, 'gi');
+      if (sitePattern.test(href)) {
+        KeywordService.keywords.push(...KeywordService.Setting.keywords[ site ]);
+      }
+    });
   }
 
   static list () {
