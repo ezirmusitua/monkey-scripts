d5fed6a6e8bfe2df8e71f420673a47493f5f3cb8
ezirmusitua <jferroal@gmail.com>
2017-11-22 11:54:03 +0800

    Update pure reading version
    Took 1 hour 3 minutes

--- pure-reading/dist/bundle.js
+++ pure-reading/dist/bundle.js
@@ -3,7 +3,7 @@
 // @name:zh-CN          阅读模式
 // @description         Update site style for reading 
 // @description:zh-CN   修改特定网站界面的样式以便阅读
-// @version             0.1.0
+// @version             0.1.1
 // @author              jferroal
 // @license             GPL-3.0
 // @updateURL           https://github.com/ezirmusitua/my-tamper-monkey-scripts/raw/master/pure-reading.user.js
