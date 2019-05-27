ad0d7d7d6cca32a9bd0411f8549c769fdf424c7e
ezirmusitua <jferroal@gmail.com>
2019-05-27 16:50:52 +0800

    Fix: Export

--- healthy-hentai/export.ps1
+++ healthy-hentai/export.ps1
@@ -3,6 +3,6 @@ echo "npx browserify ./src/index.js -o ./dist/bundle.js"
 npx browserify .\src\index.js -o .\dist\bundle.js
 echo " = = = = = Browserify done = = = = = = "
 echo "prepend greasyfork head "
-sed -i '1s/^/\/\/==UserScript==\n\/\/@name                Healthy Hentai\n\/\/@name:zh-CN          Healthy Hentai\n\/\/@description         Remove Bloody Galleries In hitomi\n\/\/@description:zh-CN   移除 hitomi 中含有血腥标签的图集\n\/\/@version             0.1.0\n\/\/@author              jferroal\n\/\/@license             GPL-3.0\n\/\/@include             https:\/\/hitomi.la\/*\n\/\/@run-at              document-idle\n\/\/@namespace           https:\/\/greasyfork.org\/users\/34556\n\/\/==\/UserScript==\n\n/g' .\dist\bundle.js
+sed -i '1s/^/\/\/ ==UserScript==\n\/\/ @name                Healthy Hentai\n\/\/ @name:zh-CN          Healthy Hentai\n\/\/ @description         Remove Bloody Galleries In hitomi\n\/\/ @description:zh-CN   移除 hitomi 中含有血腥标签的图集\n\/\/ @version             0.1.0\n\/\/ @author              jferroal\n\/\/ @license             GPL-3.0\n\/\/ @include             https:\/\/hitomi.la\/*\n\/\/ @run-at              document-idle\n\/\/ @namespace           https:\/\/greasyfork.org\/users\/34556\n\/\/ ==\/UserScript==\n\n/g' .\dist\bundle.js
 echo " = = = = = prepend bundle with greasyfork head done = = = = = "
 cp .\dist\bundle.js .\export\healthy-hentai.user.js
