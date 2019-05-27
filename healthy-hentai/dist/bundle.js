fe5cc9c6c546f9d32a65946704498d1702ec012c
ezirmusitua <jferroal@gmail.com>
2019-05-27 16:46:40 +0800

    Fix: Export

--- healthy-hentai/dist/bundle.js
+++ healthy-hentai/dist/bundle.js
@@ -1,15 +1,15 @@
 //==UserScript==
-// @name                Healthy Hentai
-// @name:zh-CN          Healthy Hentai
-// @description         Remove Bloody Galleries In hitomi
-// @description:zh-CN   移除 hitomi 中含有血腥标签的图集
-// @version             0.1.0
-// @author              jferroal
-// @license             GPL-3.0
-// @include             https://hitomi.la/*
-// @run-at              document-idle
-// @namespace           https://greasyfork.org/users/34556
-// ==/UserScript==
+//@name                Healthy Hentai
+//@name:zh-CN          Healthy Hentai
+//@description         Remove Bloody Galleries In hitomi
+//@description:zh-CN   移除 hitomi 中含有血腥标签的图集
+//@version             0.1.0
+//@author              jferroal
+//@license             GPL-3.0
+//@include             https://hitomi.la/*
+//@run-at              document-idle
+//@namespace           https://greasyfork.org/users/34556
+//==/UserScript==
 
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 (function () {
